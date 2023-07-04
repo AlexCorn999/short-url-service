e89959ada910b3c313ca6a7c1cb006c8c14bd5cf
AlexCorn999 <alexcorn999@list.ru>
2023-07-04 20:52:21 +0300

    new

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/AlexCorn999/short-url-service/internal/app/handlers"
@@ -13,5 +12,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", store.StringAcceptAndBack)
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
 }
