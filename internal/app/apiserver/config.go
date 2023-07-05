60f982e4b32567d1932fabc5bbeecae21330f9ce
AlexCorn999 <alexcorn999@list.ru>
2023-07-05 13:58:35 +0300

    fixed addr flag

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,6 +1,11 @@
 package apiserver
 
-import "flag"
+import (
+	"errors"
+	"flag"
+	"strconv"
+	"strings"
+)
 
 type Config struct {
 	bindAddr     string
@@ -12,16 +17,42 @@ func NewConfig() *Config {
 	return &Config{}
 }
 
+type NetAddress struct {
+	Host string
+	Port int
+}
+
+func (a NetAddress) String() string {
+	return a.Host + ":" + strconv.Itoa(a.Port)
+}
+
+func (a *NetAddress) Set(s string) error {
+	hp := strings.Split(s, ":")
+	if len(hp) != 2 {
+		return errors.New("Need address in a form host:port")
+	}
+	port, err := strconv.Atoi(hp[1])
+	if err != nil {
+		return err
+	}
+	a.Host = hp[0]
+	a.Port = port
+	return nil
+}
+
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func (c *Config) parseFlags() {
+	// адрес по умолчанию
+	c.bindAddr = ":8080"
 
-	// регистрируем переменную flagRunAddr
-	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&c.bindAddr, "a", ":8080", "address and port to run server")
-	// регистрируем переменную flagRunAddr
-	// как аргумент -a со значением :8080 по умолчанию
+	addr := new(NetAddress)
+	_ = flag.Value(addr)
+
+	flag.Var(addr, "a", "Net address host:port")
 	flag.StringVar(&c.shortURLAddr, "b", "localhost:8080", "address and port for short URL")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	c.bindAddr = addr.String()
 }
