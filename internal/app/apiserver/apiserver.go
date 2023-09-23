705471e3e5e3b79b12e698392c220a1f9db515bf
AlexCorn999 <alexcorn999@list.ru>
2023-09-23 20:15:15 +0800

    a

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -517,6 +517,7 @@ func (s *APIServer) GetAllURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(objectJSON)
 }
