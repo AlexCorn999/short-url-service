60a5b1490acb6a644327d4a64995a8564f14bf4c
AlexCorn999 <alexcorn999@list.ru>
2023-10-01 13:46:34 +0300

    file problems

--- internal/app/filestorage/filestorage.go
+++ internal/app/filestorage/filestorage.go
@@ -49,8 +49,7 @@ func (d *BoltDB) WriteURL(url *store.URL, id int, ssh *string) error {
 	d.Store.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("URLBucket"))
 		err := b.Put([]byte(*ssh), data)
-		return err
-		//fmt.Errorf("error from file bucket. can't put url to bucket - %s ", err)
+		return fmt.Errorf("error from file. can't put url to bucket - %s ", err)
 	})
 	return nil
 }
@@ -66,7 +65,7 @@ func (d *BoltDB) ReadURL(url *store.URL, ssh string) error {
 
 	if err := json.Unmarshal(v, url); err != nil {
 		return err
-		//fmt.Errorf("error from file bucket. can't convert url from bucket - %s ", err)
+		//fmt.Errorf("error from file. can't convert url from bucket - %s ", err)
 	}
 
 	if url.DeletedFlag {
@@ -86,7 +85,7 @@ func (d *BoltDB) GetAllURL(id int) ([]store.URL, error) {
 		for k, value := c.First(); k != nil; k, value = c.Next() {
 			var url store.URL
 			if err := json.Unmarshal([]byte(value), &url); err != nil {
-				return fmt.Errorf("error from file bucket. can't convert url from bucket - %s ", err)
+				return fmt.Errorf("error from file. can't convert url from bucket - %s ", err)
 			}
 			if url.Creator == id {
 				userURL = append(userURL, url)
@@ -115,14 +114,14 @@ func (d *BoltDB) DeleteURL(shortURL string, creator int) error {
 		for k, value := c.First(); k != nil; k, value = c.Next() {
 			var url store.URL
 			if err := json.Unmarshal([]byte(value), &url); err != nil {
-				return fmt.Errorf("error from file bucket. can't convert url from bucket - %s ", err)
+				return fmt.Errorf("error from file. can't convert url from bucket - %s ", err)
 			}
 
 			if url.ShortURL == shortURL && url.Creator == creator {
 				url.DeletedFlag = true
 				data, err := json.Marshal(url)
 				if err != nil {
-					return fmt.Errorf("error from file bucket. can't convert url from bucket - %s ", err)
+					return fmt.Errorf("error from file. can't convert url for bucket - %s ", err)
 				}
 
 				var val valuesForDelete
